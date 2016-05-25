1594203edf1d78796584ee0f1f9f7b7a33aacb3a
Kaligor <Kaligor5@hotmail.com>
2016-05-25 14:13:34 +0200

    Construction Yard again

--- src/java/com/Eagle/Admin/View/ConstructionYard.java
+++ src/java/com/Eagle/Admin/View/ConstructionYard.java
@@ -24,7 +24,7 @@ public class ConstructionYard
     String description = "";
     List<Plane> hangar = new ArrayList();
     Plane plane;
-    List<Plane> allPlanes = new ArrayList<>();
+    List<Plane> allPlanes;
 
     public void buildAirport()
     {
@@ -49,7 +49,7 @@ public class ConstructionYard
     //<editor-fold defaultstate="collapsed" desc="Getters & Setters">
     public List<Plane> getAllPlanes()
     {
-        return allPlanes = logic.getAllPlanes();
+        return allPlanes;
     }
 
     public Airport getAirport()
