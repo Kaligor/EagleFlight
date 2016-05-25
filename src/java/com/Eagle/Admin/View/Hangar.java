eacdfa29c44fb3be664c5c719b5b480c8eaa8774
Kaligor <Kaligor5@hotmail.com>
2016-05-25 10:34:12 +0200

    Hotfix
    Small update to Hangar

--- src/java/com/Eagle/Admin/View/Hangar.java
+++ src/java/com/Eagle/Admin/View/Hangar.java
@@ -34,4 +34,58 @@ public class Hangar
     {
         logic.removePlane(id);
     }
+
+    //<editor-fold defaultstate="collapsed" desc="Getters & Setters">
+    public ALogic getLogic()
+    {
+        return logic;
+    }
+    
+    public void setLogic(ALogic logic)
+    {
+        this.logic = logic;
+    }
+    
+    public long getId()
+    {
+        return id;
+    }
+    
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+    
+    public String getCallsign()
+    {
+        return callsign;
+    }
+    
+    public void setCallsign(String callsign)
+    {
+        this.callsign = callsign;
+    }
+    
+    public int getNrOfF()
+    {
+        return nrOfF;
+    }
+    
+    public void setNrOfF(int nrOfF)
+    {
+        this.nrOfF = nrOfF;
+    }
+    
+    public int getNrOfE()
+    {
+        return nrOfE;
+    }
+    
+    public void setNrOfE(int nrOfE)
+    {
+        this.nrOfE = nrOfE;
+    }
+//</editor-fold>
+    
+    
 }
